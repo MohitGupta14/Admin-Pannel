5b585864b0d86e756e12cf2e95c7e9ab1614a578
Mohit Gupta <mohitgupta@Mohits-MacBook-Air-3.local>
2023-11-27 20:10:06 +0530

    Final commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>IamAdmin</span>
+      <span>© Mohit Admin Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
